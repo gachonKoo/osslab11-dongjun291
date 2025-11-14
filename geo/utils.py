e253f0f77d671c7b4c08d035d7616fb00e5f1c29
dongjun291 <tiranojang@gachon.ac.kr>
2025-11-14 17:03:14 +0900

    Finish geo package lab11

---
+++ geo/utils.py
@@ -0,0 +1,8 @@
+def circle_area(r):
+    return 3.14 * r * r
+
+def rect_area(w, h):
+    return w * h
+
+def tri_area(w, h):
+    return 0.5 * w * h
---
+++ tester.py
@@ -0,0 +1,9 @@
+from geo.utils import circle_area, rect_area, tri_area
+
+def main():
+    print(circle_area(10))
+    print(rect_area(3, 4))
+    print(tri_area(4, 5))
+
+if __name__ == "__main__":
+    main()
